8cf0ddfd960fabd67efd545bd4c91571225398c9
Archit Jindal <59136349+architjindalcs@users.noreply.github.com>
2021-12-02 16:53:14 +0530

    Time: 4 ms (99.84%), Space: 13 MB (97.03%) - LeetHub

--- 4sum/4sum.cpp
+++ 4sum/4sum.cpp
@@ -1,44 +1,33 @@
 class Solution {
 public:
-    vector<vector<int>> kSum(vector<int>& nums,int idx,int target,int k){
-       
-        if(k==2){
-            //the problem has been reduced to two sum..
-            vector<vector<int>> ans;
-            int l=idx,r=nums.size()-1;
-            while(l<r){
-                int sum=nums[l]+nums[r];
-                if(sum==target){
-                    ans.push_back({nums[l],nums[r]});
-                    int ele1=nums[l];
-                    while(l<r and nums[l]==ele1) l++;
-                    int ele2=nums[r];
-                    while(l<r and nums[r]==ele2) r--;
-                    
-                }
-                else if(sum<target) l++;
-                else r--;
-            }
-            return ans;
-        }
-        vector<vector<int>> ans;
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        sort(nums.begin(),nums.end());
+        if(nums.size()<=3) return {};
         int n=nums.size();
-        for(int i=idx;i<n-k+1;i++)
-        {
-            if(i>idx and nums[i]==nums[i-1]) continue;
-            vector<vector<int>> next=kSum(nums,i+1,target-nums[i],k-1);
-            for(auto v: next){
-                vector<int> temp;
-                temp.push_back(nums[i]);
-                for(int i: v) temp.push_back(i);
-                ans.push_back(temp);
+        vector<vector<int>> ans;
+        for(int i=0;i<nums.size()-3;i++){
+            if(i and nums[i]==nums[i-1]) continue; //case of repetition..
+            if((long)nums[i]+nums[i+1]+nums[i+2]+nums[i+3]>(long)target) break;
+            if((long)nums[i]+nums[n-3]+nums[n-2]+nums[n-1]<(long)target) continue;
+            for(int j=i+1;j<nums.size()-2;j++){
+                if(j>i+1 and nums[j]==nums[j-1]) continue;
+                if((long)nums[i]+nums[j]+nums[j+1]+nums[j+2]>(long)target) break;
+                if((long)nums[i]+nums[j]+nums[n-2]+nums[n-1]<(long)target) continue;
+                int l=j+1,r=n-1;
+                while(l<r){
+                    int sum=nums[l]+nums[r]+nums[i]+nums[j];
+                    if(sum==target){
+                        ans.push_back({nums[i],nums[j],nums[l],nums[r]});
+                        l++;
+                        r--;
+                        while(l<nums.size() and nums[l]==nums[l-1]) l++;
+                        while(r>l and nums[r]==nums[r+1]) r--;
+                    }
+                    else if(sum<target) l++;
+                    else r--;
+                }
             }
         }
         return ans;
-    }
-    vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        if(nums.size()<=3) return {};
-        sort(nums.begin(),nums.end());
-        return kSum(nums,0,target,4);
     }
 };
