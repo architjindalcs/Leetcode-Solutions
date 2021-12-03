0c2a0d54a5d8fc4dc3c46563f2c152a0f63512db
Archit Jindal <59136349+architjindalcs@users.noreply.github.com>
2021-12-03 16:47:11 +0530

    Time: 68 ms (90.63%), Space: 11.7 MB (11.80%) - LeetHub

--- count-number-of-teams/count-number-of-teams.cpp
+++ count-number-of-teams/count-number-of-teams.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
     int numTeams(vector<int>& nums) {
-        int ans=0,n=nums.size();
-        for(int i=1;i<n-1;i++){
-            int lsm=0,lgt=0;
-            for(int j=i-1;j>=0;j--){
-                if(nums[i]>nums[j]) lsm++;
-                else if(nums[i]<nums[j]) lgt++;
-            }
-            int rsm=0,rgt=0;
-            for(int j=i+1;j<n;j++){
-                if(nums[i]>nums[j]) rsm++;
-                else if(nums[i]<nums[j]) rgt++;
-            }
-            ans+=(lsm*rgt)+(lgt*rsm);
+        set<int> lset;
+        set<int> rset(nums.begin(),nums.end());
+        int res=0;
+        for(int i=1;i<nums.size();i++){
+            lset.insert(nums[i-1]);
+            rset.erase(nums[i-1]); 
+            //CASE 1 -> nums[i]<nums[j]<nums[k];
+            //1,2, 3
+            int d1=distance(lset.begin(),lset.lower_bound(nums[i]));
+            int d2=distance(rset.upper_bound(nums[i]),rset.end());
+            if(d1 > 0 && d2 > 0)
+                res += d1*d2;
+            
+            int d3 = std::distance(lset.upper_bound(nums[i]), lset.cend()); 
+            int d4 = std::distance(rset.cbegin(), rset.lower_bound(nums[i])); 
+            if(d3 > 0 && d4 > 0)
+                res += d3*d4; 
         }
-        return ans;
+        return res;
     }
 };
