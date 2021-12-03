e7495de04e6fcab281a7445f7f8989d2c630b8c4
Archit Jindal <59136349+architjindalcs@users.noreply.github.com>
2021-12-03 17:02:25 +0530

    Time: 4 ms (70.50%), Space: 8 MB (38.88%) - LeetHub

---
+++ permutations/permutations.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    void helper(vector<int>& nums,int idx){
+        if(idx==nums.size()){
+            ans.push_back(nums);
+            return;
+        }
+        for(int i=idx;i<nums.size();i++){
+            swap(nums[idx],nums[i]);
+            helper(nums,idx+1);
+            swap(nums[idx],nums[i]);
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        helper(nums,0);
+        return ans;
+    }
+};
