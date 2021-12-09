8a324ddba9283ac23540390a87cf8daad707b663
Archit Jindal <59136349+architjindalcs@users.noreply.github.com>
2021-12-09 21:30:12 +0530

    Time: 4 ms (99.38%), Space: 10.2 MB (49.28%) - LeetHub

--- stone-game-ii/stone-game-ii.cpp
+++ stone-game-ii/stone-game-ii.cpp
@@ -5,19 +5,20 @@ public:
     int helper(vector<int>& piles,int idx,int M){
         if(idx==piles.size()) return 0;
         int end=idx+2*M-1;
-        if(end>=piles.size()-1) return suff[idx]; //alice can them all..haha
+        if(end>=piles.size()-1) return suff[idx];
         if(dp[idx][M]!=-1) return dp[idx][M];
         
         int minm=INT_MAX;
         for(int x=1;x<=2*M;x++){
             minm=min(minm,helper(piles,idx+x,max(M,x)));
         }
-        return dp[idx][M]=(suff[idx]-minm);
+        return dp[idx][M]=suff[idx]-minm;
     }
     int stoneGameII(vector<int>& piles) {
         int n=piles.size();
-        dp=vector<vector<int>> (n,vector<int>(n,-1));
-        suff=vector<int> (n,0);
+        dp=vector<vector<int>>(n,vector<int>(n,-1));
+        
+        suff=vector<int> (n);
         suff[n-1]=piles[n-1];
         for(int i=n-2;i>=0;i--)
             suff[i]=piles[i]+suff[i+1];
