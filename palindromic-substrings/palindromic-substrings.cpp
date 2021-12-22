037a7404216c2a3931a83cf06a61256785402de0
Archit Jindal <59136349+architjindalcs@users.noreply.github.com>
2021-12-22 14:24:22 +0530

    Time: 4 ms (88.54%), Space: 6.3 MB (83.62%) - LeetHub

---
+++ palindromic-substrings/palindromic-substrings.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int countSubstrings(string s) {
+        int ans=0;
+        for(int i=0;i<s.size();i++){
+            int l=i,r=i;
+            while(l>=0 and r<s.size() and s[l]==s[r]){
+                ans++;
+                l--;
+                r++;
+            }
+            l=i,r=i+1;
+            while(l>=0 and r<s.size() and s[l]==s[r]){
+                ans++;
+                l--;
+                r++;
+            }
+        }
+        return ans;
+    }
+};
