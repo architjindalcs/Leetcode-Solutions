30cb79be1c2fe5abc189f9dd610b4102f6b0ffd9
Archit Jindal <59136349+architjindalcs@users.noreply.github.com>
2022-01-05 11:21:51 +0530

    Time: 6 ms (30.62%), Space: 6.6 MB (40.86%) - LeetHub

---
+++ 299-bulls-and-cows/299-bulls-and-cows.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string getHint(string secret, string guess) {
+        int f[10];
+        memset(f,0,sizeof(f));
+        for(char ch: secret) f[ch-'0']++;
+        int equal=0,cbs=0;
+        for(int i=0;i<guess.size();i++){
+            if(secret[i]==guess[i]){
+                int c=secret[i]-'0';
+                f[c]--;
+                equal++;
+            }
+        }
+        for(int i=0;i<guess.size();i++){
+            if(secret[i]==guess[i]) {
+                
+            }
+            else{
+                int c=guess[i]-'0';
+                if(f[c]){
+                    cbs++;
+                    f[c]--;
+                }
+            }
+        }
+        return to_string(equal)+"A"+to_string(cbs)+"B";
+        
+    }
+};
