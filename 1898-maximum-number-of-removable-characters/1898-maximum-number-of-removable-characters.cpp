f440f910e8f33988071d7ac540ab5ce55e6c0f18
Archit Jindal <59136349+architjindalcs@users.noreply.github.com>
2022-01-06 12:09:35 +0530

    Time: 629 ms (19.26%), Space: 75.9 MB (95.56%) - LeetHub

---
+++ 1898-maximum-number-of-removable-characters/1898-maximum-number-of-removable-characters.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool poss(string& s,string& p,vector<bool>& rmv){
+        int i=0,j=0;
+        while(i<s.size() and j<p.size()){
+            if(rmv[i]){
+                i++;
+                continue;
+            }
+            if(s[i]==p[j]){
+                i++;
+                j++;
+            }
+            else i++;
+        }
+        return j==p.size();
+    }
+    int maximumRemovals(string s, string p, vector<int>& removable) {
+        int st=0,end=removable.size();
+        int ans;
+        int n=s.size();
+         vector<bool> rmv(n,false); //rmv[i]= true, we are removing the  ith index..
+        while(st<=end){
+            int mid=(st+end)/2;
+            for(int i=0;i<mid;i++){
+                rmv[removable[i]]=true;
+            }
+            if(poss(s,p,rmv)){
+                ans=mid;
+                st=mid+1;
+            }
+            else end=mid-1;
+            for(int i=0;i<mid;i++){
+                rmv[removable[i]]=false;
+            }
+        }
+        return ans;
+    }
+};
