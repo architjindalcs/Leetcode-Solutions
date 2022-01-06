2aefec60bbd761b37a231bc7cc66ecc13f63d613
Archit Jindal <59136349+architjindalcs@users.noreply.github.com>
2022-01-06 10:10:43 +0530

    Time: 384 ms (7.48%), Space: 38.9 MB (37.38%) - LeetHub

---
+++ 731-my-calendar-ii/731-my-calendar-ii.cpp
@@ -0,0 +1,29 @@
+class MyCalendarTwo {
+public:
+    map<int,int> m;
+    MyCalendarTwo() {
+        
+    }
+    
+    bool book(int start, int end) {
+        end--;
+        m[start]++;
+        m[end+1]--;
+        int cs=0;
+        for(auto& x: m){
+            cs+=(x.second);
+            if(cs>=3) {
+                m[start]--;
+                m[end+1]++;
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+/**
+ * Your MyCalendarTwo object will be instantiated and called as such:
+ * MyCalendarTwo* obj = new MyCalendarTwo();
+ * bool param_1 = obj->book(start,end);
+ */
