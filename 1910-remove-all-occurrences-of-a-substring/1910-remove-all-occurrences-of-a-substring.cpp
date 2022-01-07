2ff10c23b34b383f3ac34437b1276dec00a470c6
Archit Jindal <59136349+architjindalcs@users.noreply.github.com>
2022-01-07 10:12:11 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (29.23%) - LeetHub

--- 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
@@ -1,21 +1,11 @@
 class Solution {
 public:
     string removeOccurrences(string s, string part) {
-        string temp;
-        while(true){
-            bool f=false;
-            temp="";
-            for(int i=0;i<s.size();i++){
-                if(!f and i+part.size()-1<=s.size()-1 and s.substr(i,(int)part.size())==part){
-                    f=true;
-                    i+=(part.size()-1);
-                }
-                else temp+=s[i];
-            }
-          //  cout<<"temp: "<<temp<<endl; 
-            if(!f) return temp;
-            s=temp;
+        int idx=s.find(part);
+        while(idx!=-1){
+            s.erase(idx,part.size());
+            idx=s.find(part);
         }
-        return "";
+        return s;
     }
 };
