31a7128ab47130f1a88f6f1685eb68b6d838499e
Archit Jindal <59136349+architjindalcs@users.noreply.github.com>
2022-01-07 22:39:45 +0530

    Time: 505 ms (13.52%), Space: 97.3 MB (59.07%) - LeetHub

--- 1424-diagonal-traverse-ii/1424-diagonal-traverse-ii.cpp
+++ 1424-diagonal-traverse-ii/1424-diagonal-traverse-ii.cpp
@@ -6,16 +6,13 @@ public:
         unordered_map<int,vector<int>> m;
         for(int i=0;i<grid.size();i++){
             for(int j=0;j<grid[i].size();j++){
-                maxm=max(maxm,(int)grid[i].size());
+                maxm=max(maxm,i+j);
                 m[i+j].push_back(grid[i][j]);
             }
         }
-        for(int i=0;i<grid.size();i++){
+
+        for(int i=0;i<=maxm;i++){
             int sum=i;
-            for(int j=m[sum].size()-1;j>=0;j--) ans.push_back(m[sum][j]);
-        }
-        for(int i=1;i<=maxm;i++){
-            int sum=i+grid.size()-1;
             for(int j=m[sum].size()-1;j>=0;j--){
                 ans.push_back(m[sum][j]);
             }
