48af3da28f5c5d28f96c6abea81b80a0555ea172
Archit Jindal <59136349+architjindalcs@users.noreply.github.com>
2022-01-08 10:34:10 +0530

    Time: 119 ms (15.30%), Space: 91.3 MB (16.85%) - LeetHub

--- 459-repeated-substring-pattern/459-repeated-substring-pattern.cpp
+++ 459-repeated-substring-pattern/459-repeated-substring-pattern.cpp
@@ -3,7 +3,7 @@ public:
     bool repeatedSubstringPattern(string s) {
         int n=s.size();
         string cs="";
-        for(int i=0;i<s.size()-1;i++){
+        for(int i=0;i<s.size()/2;i++){
             cs+=s[i];
             int csl=cs.size();
             if(n%csl==0){
