c211945fae41fc1743df82326ab0bf1d97145ebc
Archit Jindal <59136349+architjindalcs@users.noreply.github.com>
2022-01-10 22:43:26 +0530

    Time: 212 ms (12.50%), Space: 84 MB (87.50%) - LeetHub

--- 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.cpp
+++ 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.cpp
@@ -4,7 +4,7 @@ public:
         int cntOnes=0;
         for(int i: nums) cntOnes+=(i==1);
         if(!cntOnes) return 0;
-        int winSize=cntOnes/2;
+        int winSize=cntOnes;
         int ans=INT_MAX;
         int cones=0;
 
@@ -12,16 +12,16 @@ public:
             cones+=(nums[i]);
         }
         ans=min(ans,winSize-cones);
-        for(int i=winSize;i<nums.size();i++){
-            cones+=(nums[i]);
-            cones-=(nums[i-winSize]);
+        for(int i=winSize;i<2*nums.size();i++){
+            cones+=(nums[i%nums.size()]);
+            cones-=(nums[(i-winSize)%nums.size()]);
             ans=min(ans,winSize-cones);
         }
         return ans; 
     }
     int minSwaps(vector<int>& nums) {
         vector<int> temp=nums;
-        for(int i: nums) temp.push_back(i);
+      //  for(int i: nums) temp.push_back(i);
         return helper(temp);
     }   
 };
