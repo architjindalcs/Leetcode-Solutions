4c4a538d7884b9f2eaf830c2e2c7d168adc78371
Archit Jindal <59136349+architjindalcs@users.noreply.github.com>
2022-01-10 22:55:43 +0530

    Time: 244 ms (41.18%), Space: 67.6 MB (41.18%) - LeetHub

--- 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.cpp
+++ 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
     bool checkValid(vector<vector<int>>& grid) {
+         unordered_set<int> st;
         for(int i=0;i<grid.size();i++){
-            unordered_set<int> st;
+           st.clear();
             for(int j=0;j<grid[i].size();j++) {
                 if(grid[i][j]>grid.size()) return false;
                 else st.insert(grid[i][j]);
@@ -13,7 +14,8 @@ public:
             for(int j=0;j<grid[i].size();j++) {
                 if(grid[j][i]>grid.size()) return false;
                 else st.insert(grid[j][i]);
-            }if(st.size()!=grid.size()) return false;
+            }
+            if(st.size()!=grid.size()) return false;
         }     
         return true;
     }
