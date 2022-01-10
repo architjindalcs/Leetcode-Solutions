a1516ae6d9297122ef33b5eb6906c2dd75d8927f
Archit Jindal <59136349+architjindalcs@users.noreply.github.com>
2022-01-10 22:34:59 +0530

    Time: 207 ms (12.50%), Space: 90.9 MB (87.50%) - LeetHub

--- 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.cpp
+++ 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.cpp
@@ -1,18 +1,13 @@
 class Solution {
 public:
-    int minSwaps(vector<int>& nums) {
+    int helper(vector<int>& nums){
         int cntOnes=0;
         for(int i: nums) cntOnes+=(i==1);
         if(!cntOnes) return 0;
-        int winSize=cntOnes;
+        int winSize=cntOnes/2;
         int ans=INT_MAX;
         int cones=0;
-        vector<int> pre;
-        int psum=0;
-        for(int i: nums) {
-            psum+=i;
-            pre.push_back(psum);
-        }
+
         for(int i=0;i<winSize;i++){
             cones+=(nums[i]);
         }
@@ -22,16 +17,11 @@ public:
             cones-=(nums[i-winSize]);
             ans=min(ans,winSize-cones);
         }
-        int pidx=1;
-        while(pidx<cntOnes){
-            int suff=cntOnes-pidx;
-            int aageSum=(pre[pidx-1]);
-            int end=nums.size()-1;
-            int st=end-suff+1;
-            int peecheSum=(pre.back()-(st? pre[st-1]: 0));
-            ans=min(ans,winSize-(aageSum+peecheSum));
-            pidx++;
-        }
-        return ans;
+        return ans; 
     }
+    int minSwaps(vector<int>& nums) {
+        vector<int> temp=nums;
+        for(int i: nums) temp.push_back(i);
+        return helper(temp);
+    }   
 };
