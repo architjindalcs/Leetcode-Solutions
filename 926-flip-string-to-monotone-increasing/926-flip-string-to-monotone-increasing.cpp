883beb89f80a0d00d46cb4b59efc314bbdcf9848
Archit Jindal <59136349+architjindalcs@users.noreply.github.com>
2022-01-15 11:17:08 +0530

    Time: 32 ms (57.51%), Space: 11 MB (80.78%) - LeetHub

--- 926-flip-string-to-monotone-increasing/926-flip-string-to-monotone-increasing.cpp
+++ 926-flip-string-to-monotone-increasing/926-flip-string-to-monotone-increasing.cpp
@@ -1,26 +1,18 @@
 class Solution {
 public:
     int minFlipsMonoIncr(string s) {
-      //aaaaaaaabbb   aaaaaaaaaaaaaaab   b
-        int n=s.size();
-        int dp[n][2];
-        memset(dp,0,sizeof(dp));
-        int ocnt=(s[0]=='1');
-        dp[0][0]=(s[0]=='1');
-        dp[0][1]=(s[0]=='0');
-        for(int i=1;i<s.size();i++){
-            //dp[i][0] -> ending with 0 only..
-            if(s[i]=='1') ocnt++;
+        int flipcnt=0,onecnt=0;
+        for(int i=0;i<s.size();i++){
             if(s[i]=='0'){
-                dp[i][0]=dp[i-1][0];  //yeh bhi zero h, just take prev value..
-                dp[i][1]=1+min(dp[i-1][0],dp[i-1][1]);
+                if(onecnt==0) continue;
+                else flipcnt++;
             }
             else{
-                //current is '1';
-                dp[i][0]=min(ocnt,1+dp[i-1][0]);
-                dp[i][1]=min(dp[i-1][0],dp[i-1][1]);
+                onecnt++;
             }
+            if(flipcnt>onecnt)
+                flipcnt=onecnt;
         }
-        return min(dp[n-1][0],dp[n-1][1]);
+        return flipcnt;
     }
 };
