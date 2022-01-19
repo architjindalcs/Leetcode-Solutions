5f56e117f24f3c5271504b9dc371655f2c7efd16
Archit Jindal <59136349+architjindalcs@users.noreply.github.com>
2022-01-19 09:03:32 +0530

    Time: 128 ms (97.76%), Space: 47.1 MB (82.27%) - LeetHub

---
+++ 802-find-eventual-safe-states/802-find-eventual-safe-states.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> color;
+    bool dfs(vector<vector<int>>& adj,int src){
+        color[src]=2; //marked initially as unsafe..
+        for(int i: adj[src]){
+            if(color[i]==2) return false; //bychance, we get here..it will lead to 
+            if(color[i]==0){
+                if(!dfs(adj,i)){
+                    return false;  //it is unsafe node...
+                }
+            }
+        }
+        color[src]=1;
+        return true;
+    }
+    vector<int> eventualSafeNodes(vector<vector<int>>& adj) {
+        int n=adj.size();
+        color=vector<int> (n,0);
+        vector<int> ans;
+        for(int i=0;i<n;i++){
+            if(color[i]==0){
+                if(dfs(adj,i)) ans.push_back(i);
+            }
+            else if(color[i]==1) ans.push_back(i);
+        }
+        return ans;
+    }
+};
