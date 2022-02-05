c97c90dfa1e901adf47504341cd20398a35cc3bf
Archit Jindal <59136349+architjindalcs@users.noreply.github.com>
2022-02-05 16:59:32 +0530

    Time: 3 ms (54.57%), Space: 8.3 MB (93.66%) - LeetHub

--- 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.cpp
@@ -9,21 +9,34 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
 class Solution {
 public:
+     TreeNode* findPred(TreeNode* root){
+        TreeNode* temp=root->left;
+        while(temp->right and temp->right!=root) temp=temp->right;
+        return temp;
+    }
     vector<int> preorderTraversal(TreeNode* root) {
-        stack<TreeNode*> st;
         TreeNode* curr=root;
         vector<int> ans;
-        while(curr or st.size()){
-            while(curr){
+        while(curr){
+            if(!(curr->left)){
                 ans.push_back(curr->val);
-                st.push(curr);
-                curr=curr->left;
+                curr=curr->right;
             }
-            curr=st.top();
-            st.pop();
-            curr=curr->right;
+            else{
+                TreeNode* pred=findPred(curr);
+                if(!(pred->right)){
+                    pred->right=curr;
+                    ans.push_back(curr->val);
+                    curr=curr->left;
+                }
+                else{
+                    pred->right=NULL;
+                    curr=curr->right;
+                }
+            }
         }
         return ans;
     }
