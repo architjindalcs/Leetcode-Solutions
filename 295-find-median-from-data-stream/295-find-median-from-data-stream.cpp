298953d5455d53b52b14580846bbad4d8bb90748
Archit Jindal <59136349+architjindalcs@users.noreply.github.com>
2022-02-08 21:28:10 +0530

    Time: 706 ms (9.03%), Space: 117.1 MB (20.59%) - LeetHub

--- 295-find-median-from-data-stream/295-find-median-from-data-stream.cpp
+++ 295-find-median-from-data-stream/295-find-median-from-data-stream.cpp
@@ -1,31 +1,31 @@
 class MedianFinder {
 public:
     priority_queue<int> maxH;
-    priority_queue<int,vector<int>,greater<int>> minH;
+    priority_queue<int,vector<int>,greater<int> > minH;
     MedianFinder() {
         
     }
     
     void addNum(int num) {
-        if(maxH.empty() or num<maxH.top()) maxH.push(num);
+        if(maxH.empty() or num<maxH.top()){
+            maxH.push(num);
+        }
         else minH.push(num);
         
-        if(((int)maxH.size()-(int)minH.size())>1) {
-            minH.push(maxH.top());
-            maxH.pop();
-        }
-         
-        else  if(((int)minH.size()-(int)maxH.size())>1){
-            //minH me jyada elements aa gye bhot..
+        int minHSize=minH.size(),maxHSize=maxH.size();
+        if(minHSize-maxHSize>1){
             maxH.push(minH.top());
             minH.pop();
         }
-        
+        else if(maxHSize-minHSize>1){
+            minH.push(maxH.top());
+            maxH.pop();
+        }
     }
     
     double findMedian() {
         return minH.size()==maxH.size()? (minH.top()+maxH.top())/2.0 : 
-        (minH.size()>maxH.size() ? minH.top() : maxH.top()); 
+        (minH.size()>maxH.size()? minH.top(): maxH.top());
     }
 };
 
