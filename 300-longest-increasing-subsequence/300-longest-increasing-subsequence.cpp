014ebc7f07670df7b36dca54d07a60f2e02a7ed2
Archit Jindal <59136349+architjindalcs@users.noreply.github.com>
2022-02-11 19:14:38 +0530

    Time: 13 ms (78.27%), Space: 10.4 MB (85.60%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -3,15 +3,15 @@ public:
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
         int ans=1;
-        int dp[n];
-        for(int i=0;i<n;i++){
-            dp[i]=1;
-            for(int j=i-1;j>=0;j--){
-                if(nums[j]<nums[i])
-                    dp[i]=max(dp[i],1+dp[j]);
+        vector<int> res;
+        for(int ele: nums){
+            if(res.empty() or res.back()<ele) 
+                res.push_back(ele);
+            else{
+                int idx=lower_bound(res.begin(),res.end(),ele)-res.begin();
+                res[idx]=ele;
             }
-            ans=max(ans,dp[i]);
         }
-        return ans;
+        return res.size();
     }
 };
