8ed2e5a324665c6c0c29d9290457ff22ef7ba7cd
Archit Jindal <59136349+architjindalcs@users.noreply.github.com>
2022-02-20 19:51:58 +0530

    Time: 244 ms (53.85%), Space: 40.7 MB (15.38%) - LeetHub

--- 2178-maximum-split-of-positive-even-integers/2178-maximum-split-of-positive-even-integers.cpp
+++ 2178-maximum-split-of-positive-even-integers/2178-maximum-split-of-positive-even-integers.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     vector<long long> maximumEvenSplit(long long finalSum) {
+        ios::sync_with_stdio(false);
+        cin.tie(NULL);
         if(finalSum%2) return {};
         vector<long long> ans;
         long long cs=0,i=2;
@@ -9,7 +11,7 @@ public:
             cs+=i;
             i+=2;
         }
-        //2,4,6,8,
+        //2,4,6,16  -> 20 28-20 = 8 -> 
         int sz=ans.size();
         ans[sz-1]+=(finalSum-cs);
         return ans;
