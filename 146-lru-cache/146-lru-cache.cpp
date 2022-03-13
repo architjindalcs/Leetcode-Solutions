cc5dadef643c71ddf550ea8215fa6abdfe7d8020
Archit Jindal <59136349+architjindalcs@users.noreply.github.com>
2022-03-13 17:20:28 +0530

    Time: 577 ms (60.68%), Space: 164.8 MB (86.99%) - LeetHub

--- 146-lru-cache/146-lru-cache.cpp
+++ 146-lru-cache/146-lru-cache.cpp
@@ -66,10 +66,3 @@ public:
         keyAddr[key]=nn;
     }
 };
-
-/**
- * Your LRUCache object will be instantiated and called as such:
- * LRUCache* obj = new LRUCache(capacity);
- * int param_1 = obj->get(key);
- * obj->put(key,value);
- */
