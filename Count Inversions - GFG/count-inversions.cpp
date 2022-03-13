b6c59910df9852454e1aad950dcf4bac88c85f19
Archit Jindal <59136349+architjindalcs@users.noreply.github.com>
2022-03-13 14:34:59 +0530

    Added solution - LeetHub

--- Count Inversions - GFG/count-inversions.cpp
+++ Count Inversions - GFG/count-inversions.cpp
@@ -8,53 +8,51 @@ class Solution{
   public:
     // arr[]: Input Array
     // N : Size of the Array arr[]
-long long merge(long long arr[],long long temp[],int l,int m,int r){
-    /*
-        1,2,  3,4
-          m
-          
-        1,2,3,    4,5
-            m
-    */
-    long long ans=0;
-    int n1=m-l+1;
-    int n2=r-m;
-    int i=l,j=m+1;
-    int k=l;
-    
-    while(i<=m and j<=r){
-        if(arr[i]<=arr[j]){
-            temp[k++]=arr[i++];
+    long long merge(long long arr[],long long temp[],int l,int m,int r){
+        /*
+            1,2,  3,4
+              m
+              
+            1,2,3,    4,5
+                m
+        */
+        long long ans=0;
+        int n1=m-l+1,n2=r-m;
+        int k=l,i=l,j=m+1;
+       while(i<=m and j<=r){
+            if(arr[i]<=arr[j]){
+                temp[k++]=arr[i++];
+            }
+            else{
+                //i se m tak elements bade hi honge 
+                ans+=(m-i+1);
+                temp[k++]=arr[j++];
+            }
         }
-        else{
-            ans+=(m+1-i);
-            temp[k++]=arr[j++];
+        while(i<=m) temp[k++]=arr[i++];
+        while(j<=r) temp[k++]=arr[j++];
+        for(int i=l;i<=r;i++) arr[i]=temp[i];
+    
+        return ans;
+        
+    }
+    long long mergeSort(long long arr[],long long temp[],int st,int end){
+        long long ans=0;
+        if(st<end){
+            int mid=(st+end)/2;
+            ans+=(mergeSort(arr,temp,st,mid));
+            ans+=(mergeSort(arr,temp,mid+1,end));
+            ans+=(merge(arr,temp,st,mid,end));
         }
+        return ans;
     }
-    while(i<=m) temp[k++]=arr[i++];
-    while(j<=r) temp[k++]=arr[j++];
-    
-    for(int i=l;i<=r;i++) arr[i]=temp[i];
-    return ans;
-    
-}
-long long mergeSort(long long arr[],long long temp[],int st,int end){
-    long long ans=0;
-    if(st<end){
-        int mid=(st+end)/2;
-        ans+=(mergeSort(arr,temp,st,mid));
-        ans+=(mergeSort(arr,temp,mid+1,end));
-        ans+=(merge(arr,temp,st,mid,end));
+    long long inversionCount(long long arr[], long long n)
+    {
+        long long temp[n];
+        memset(temp,0,sizeof(temp));
+        return mergeSort(arr,temp,0,n-1);
+        // Your Code Here
     }
-    return ans;
-}
-long long inversionCount(long long arr[], long long n)
-{
-    long long temp[n];
-    memset(temp,0,sizeof(temp));
-    return mergeSort(arr,temp,0,n-1);
-    // Your Code Here
-}
 
 };
 
