dabcfa095cc22cfb9c227eea8bd163891bbe2562
Archit Jindal <59136349+architjindalcs@users.noreply.github.com>
2022-03-14 18:55:59 +0530

    Added solution - LeetHub

--- K-th element of two sorted Arrays - GFG/kth-element-of-two-sorted-arrays.cpp
+++ K-th element of two sorted Arrays - GFG/kth-element-of-two-sorted-arrays.cpp
@@ -8,25 +8,51 @@ class Solution{
     public:
     int kthElement(int arr1[], int arr2[], int n, int m, int k)
     {
-        //n-> size of first array, m-> size of second array..
-        if(n>m)
-        return kthElement(arr2,arr1,m,n,k);
-        
+        if(n>m) return kthElement(arr2,arr1,m,n,k);  
         int st=max(0,k-m),end=min(k,n);
-        
         while(st<=end){
             int cut1=(st+end)/2;
             int cut2=k-cut1;
-            int l1=cut1? arr1[cut1-1]: INT_MIN;
-            int l2=cut2? arr2[cut2-1]: INT_MIN;
-            int r1=cut1<n? arr1[cut1]: INT_MAX;
-            int r2=cut2<m? arr2[cut2]: INT_MAX;
-            if(l1<=r2 and l2<=r1) return max(l1,l2);
+            int l1=cut1? arr1[cut1-1] : INT_MIN;
+            int l2=cut2? arr2[cut2-1] : INT_MIN;
+            int r1=cut1<n ? arr1[cut1] : INT_MAX;
+            int r2=cut2<m ? arr2[cut2] : INT_MAX;
+            
+            if(l1<=r2 and l2<=r1){
+                return max(l1,l2);
+            }
             else if(l1>r2){
                 end=cut1-1;
             }
             else st=cut1+1;
         }
+        return -1;
+        
+        //n-> size of first array, m-> size of second array..
+        // if(n>m)
+        // return kthElement(arr2,arr1,m,n,k);
+        
+        // int st=max(0,k-m),end=min(k,n);
+        // /*
+        //      st=max(0,7-1) say I need to find the 7th maxm and m=5 (size of second array),
+        //      then st=max(0,7-5) = 2  -> at least 2 elements from arr1 is to be taken.
+             
+        //      k=10, n=7 (size of first array)
+        //      then end=min(10,7) -> 7 to check the maxm elements that can be taken from arr1
+        // */
+        // while(st<=end){
+        //     int cut1=(st+end)/2;
+        //     int cut2=k-cut1;
+        //     int l1=cut1? arr1[cut1-1]: INT_MIN;
+        //     int l2=cut2? arr2[cut2-1]: INT_MIN;
+        //     int r1=cut1<n? arr1[cut1]: INT_MAX;
+        //     int r2=cut2<m? arr2[cut2]: INT_MAX;
+        //     if(l1<=r2 and l2<=r1) return max(l1,l2);
+        //     else if(l1>r2){
+        //         end=cut1-1;
+        //     }
+        //     else st=cut1+1;
+        // }
         return -1;
     }
 };
