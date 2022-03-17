e3910e2baf88ad2b6b921193a4cec45a543911ab
Archit Jindal <59136349+architjindalcs@users.noreply.github.com>
2022-03-17 15:03:16 +0530

    Time: 3 ms (96.40%), Space: 8 MB (36.27%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,33 +1,33 @@
 class Solution {
 public:
     vector<vector<string>> ans;
-    vector<string> stringify(vector<vector<int>>& grid){
+    vector<string> sfy(vector<vector<int>>& grid){
         vector<string> ans;
-        for(vector<int>& v: grid){
-            string temp="";
-            for(int i: v) temp.push_back(i? 'Q': '.');
-            ans.push_back(temp);
+        for(auto& v: grid){
+            string s="";
+            for(int i: v) if(i) s+="Q"; else s+=".";
+            ans.push_back(s);
         }
         return ans;
     }
+    
     void helper(vector<vector<int>>& grid,int row,int cm,int ldm,int rdm){
-        if(row==(grid.size())){
-            ans.push_back(stringify(grid));
+        if(row==grid.size()){
+            ans.push_back(sfy(grid));
             return;
         }
-        for(int col=0;col<grid.size();col++){
-            if(cm&(1<<col)) continue;  //already occupied column previously..
-            bool c1=(ldm&(1<<(row-col+grid.size()-1)));
-            bool c2=(rdm&(1<<(row+col)));
+        for(int col=0;col<grid[0].size();col++){
+            if(cm&(1<<col)) continue;
+            bool c1=(rdm&(1<<(row+col)));
+            bool c2=(ldm&(1<<(row-col+grid.size()-1)));
             if(!c1 and !c2){
                 grid[row][col]=1;
-                helper(grid,row+1,cm | (1<<col),(ldm|(1<<(row-col+grid.size()-1))),rdm|(1<<(row+col)));
+                helper(grid,row+1,cm|(1<<col),ldm|(1<<(row-col+grid.size()-1)), rdm | (1<<(row+col)));
                 grid[row][col]=0;
             }
         }
     }
     vector<vector<string>> solveNQueens(int n) {
-        //3,3 -> 2,2
         vector<vector<int>> grid(n,vector<int>(n,0));
         helper(grid,0,0,0,0);
         return ans;
