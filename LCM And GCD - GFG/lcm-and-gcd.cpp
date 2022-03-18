4294bd5ae05947d85c0be11986e287b7e5281803
Archit Jindal <59136349+architjindalcs@users.noreply.github.com>
2022-03-18 11:42:00 +0530

    Added solution - LeetHub

--- LCM And GCD - GFG/lcm-and-gcd.cpp
+++ LCM And GCD - GFG/lcm-and-gcd.cpp
@@ -5,15 +5,23 @@ using namespace std;
  // } Driver Code Ends
 class Solution {
   public:
-  
+    long long gcd2(long long A,long long B){
+        if(A==0) return B;
+        if(B==0) return A;
+        
+        if(A==B) return A;
+        if(A>B) return gcd2(A-B,B);
+        return gcd2(A,B-A);
+    }
     long long gcd(long long A,long long B){
         if(B==0) return A;
+        //Time complexity is O(log(max(A,B)));
         return gcd(B,A%B);
     }
     vector<long long> lcmAndGcd(long long A , long long B) {
         // code here
         vector<long long> ans;
-        long long gc=gcd(A,B);
+        long long gc=gcd2(A,B);
         long long lc=((A*B)/gc);
         return {lc,gc};
         
