5e3401451a79aa1b53c16b52be06f0abeb30863e
Archit Jindal <59136349+architjindalcs@users.noreply.github.com>
2022-03-18 13:50:40 +0530

    Added solution - LeetHub

---
+++ Find Prime numbers in a range - GFG/find-prime-numbers-in-a-range.cpp
@@ -0,0 +1,43 @@
+// { Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    vector<int> primeRange(int M, int N) {
+        // code here
+        if(M>N) return primeRange(N,M);
+        bool prime[N+1];
+        memset(prime,true,sizeof(prime));
+        prime[1]=false;
+        for(int i=2;i*i<=N;i++){
+            if(!prime[i]) continue;  
+            for(int j=i*i;j<=N;j+=i){
+                prime[j]=false;
+            }
+        }
+        vector<int> ans;
+        for(int i=M;i<=N;i++) if(prime[i]) ans.push_back(i);
+        return ans;
+    }
+};
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N, M, K;
+        cin >> M >> N;
+        Solution ob;
+        vector<int> ans = ob.primeRange(M, N);
+        for (auto u : ans) cout << u << " ";
+        cout << "\n";
+    }
+}  // } Driver Code Ends
