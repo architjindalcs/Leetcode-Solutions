b164f6b667cad0ca44a462dbb7115a2f376d8654
Archit Jindal <59136349+architjindalcs@users.noreply.github.com>
2022-03-20 13:55:26 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
@@ -8,45 +8,76 @@ using namespace std;
 class Solution
 {
 	public:
+	int minDist(vector<vector<int>>& gp,vector<int>& dist,vector<bool>& inMST){
+	    int V=gp.size();
+	    int idx=-1;
+	    int minm=INT_MAX;
+	    for(int i=0;i<V;i++){
+	        if(!inMST[i] and dist[i]<minm){
+	            minm=dist[i];
+	            idx=i;
+	        }
+	    }
+	    return idx;
+	}
 	//Function to find sum of weights of edges of the Minimum Spanning Tree.
     int spanningTree(int V, vector<vector<int>> adj[])
     {
-                vector<vector<int>> gp(V,vector<int>(V,0));
+        vector<vector<int>> gp(V,vector<int>(V,0));
         for(int i=0;i<V;i++){
             for(auto& v: adj[i]){
                 gp[i][v[0]]=v[1];
                 gp[v[0]][i]=v[1];
             }
         }
-        int parent[V];
-        memset(parent,-1,sizeof(parent));
-        bool inMST[V];
-        memset(inMST,false,sizeof(inMST));
+        vector<bool> inMST(V,false);;
         vector<int> dist(V,INT_MAX);
         dist[0]=0;
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>> > pq;
-        pq.push({dist[0],0});
-        // code here
-        while(pq.size()){
-            int frNode=pq.top().second;
-            pq.pop();
-            inMST[frNode]=true;
-            for(vector<int>& p: adj[frNode]){
-                int nodeVal=p[0], wt=p[1];
-                if(dist[nodeVal]>wt and !inMST[nodeVal]){
-                    dist[nodeVal]=wt;
-                    parent[nodeVal]=frNode;
-                    pq.push({dist[nodeVal],nodeVal});
+        vector<int> parent(V,-1);
+        for(int i=0;i<V-1;i++){
+            int u=minDist(gp,dist,inMST);
+            inMST[u]=true;
+            
+            for(int v=0;v<V;v++){
+                if(!inMST[v] and gp[u][v] and dist[v]>gp[u][v]){
+                    dist[v]=gp[u][v];
+                    parent[v]=u;
                 }
             }
         }
         int ans=0;
-        for(int i=0;i<V;i++){
-            if(parent[i]!=-1){
-                ans+=(gp[i][parent[i]]);
-            }
-        }
+        for(int i=0;i<V;i++) if(parent[i]!=-1) ans+=(gp[i][parent[i]]);
+        
         return ans;
+        // int parent[V];
+        // memset(parent,-1,sizeof(parent));
+        // bool inMST[V];
+        // memset(inMST,false,sizeof(inMST));
+        // vector<int> dist(V,INT_MAX);
+        // dist[0]=0;
+        // priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>> > pq;
+        // pq.push({dist[0],0});
+        // // code here
+        // while(pq.size()){
+        //     int frNode=pq.top().second;
+        //     pq.pop();
+        //     inMST[frNode]=true;
+        //     for(vector<int>& p: adj[frNode]){
+        //         int nodeVal=p[0], wt=p[1];
+        //         if(dist[nodeVal]>wt and !inMST[nodeVal]){
+        //             dist[nodeVal]=wt;
+        //             parent[nodeVal]=frNode;
+        //             pq.push({dist[nodeVal],nodeVal});
+        //         }
+        //     }
+        // }
+        // int ans=0;
+        // for(int i=0;i<V;i++){
+        //     if(parent[i]!=-1){
+        //         ans+=(gp[i][parent[i]]);
+        //     }
+        // }
+        // return ans;
     }
 };
 
