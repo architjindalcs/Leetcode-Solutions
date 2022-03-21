ff23efa644cf52d26029e5c08596dd2e17efcbef
Archit Jindal <59136349+architjindalcs@users.noreply.github.com>
2022-03-21 12:31:35 +0530

    Time: 82 ms (88.41%), Space: 11.7 MB (11.67%) - LeetHub

--- 1395-count-number-of-teams/1395-count-number-of-teams.cpp
+++ 1395-count-number-of-teams/1395-count-number-of-teams.cpp
@@ -9,13 +9,18 @@ public:
             rset.erase(nums[i-1]); 
             //CASE 1 -> nums[i]<nums[j]<nums[k];
             //1,2, 3
-            int d1=distance(lset.begin(),lset.lower_bound(nums[i]));
-            int d2=distance(rset.upper_bound(nums[i]),rset.end());
-            if(d1 > 0 && d2 > 0)
-                res += d1*d2;
+            //left me chote..
+            int cnt1=distance(lset.begin(),lset.upper_bound(nums[i]));
+            //right me bde jisme element bhi h saath
+            int cnt2=distance(rset.find(nums[i]),rset.end())-1;
             
-            int d3 = distance(lset.upper_bound(nums[i]), lset.end()); 
-            int d4 = distance(rset.begin(), rset.find(nums[i])); 
+            // int d1=distance(lset.begin(),lset.lower_bound(nums[i])); 
+            // int d2=distance(rset.upper_bound(nums[i]),rset.end());
+            if(cnt1 > 0 && cnt2 > 0)
+                res += cnt1*cnt2;
+            
+            int d3 = distance(lset.lower_bound(nums[i]), lset.end());  //left be bde..
+            int d4 = distance(rset.begin(), rset.find(nums[i])); //right me chote..
             if(d3 > 0 && d4 > 0)
                 res += d3*d4; 
         }
