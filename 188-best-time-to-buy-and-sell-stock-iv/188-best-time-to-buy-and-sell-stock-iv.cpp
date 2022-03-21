2be5abe125c340ccf9fe42e57b4c6f848f3cdbae
Archit Jindal <59136349+architjindalcs@users.noreply.github.com>
2022-03-21 17:23:24 +0530

    Time: 13 ms (55.19%), Space: 11.1 MB (70.03%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -9,34 +9,15 @@ public:
         for(int kval=1;kval<=k;kval++){
             //kval-> no of transactions we can do
             dp[kval][0]=0;  //no profit possible...
+            int maxm=dp[kval-1][0]-prices[0]; //-prices[0] effectively..
             for(int i=1;i<n;i++){
                 int dntst=dp[kval][i-1];
-                int maxm=INT_MIN;
-                for(int j=i-1;j>=0;j--){
-                    maxm=max(maxm,dp[kval-1][j]+(prices[i]-prices[j]));
-                }
-                dp[kval][i]=max(dp[kval][i-1],maxm);
+                int tst=prices[i]+maxm;
+                dp[kval][i]=max(dntst,tst);
+                maxm=max(maxm,dp[kval-1][i]-prices[i]);
             }
-            
         }
         
         return dp[k][n-1];
-        // if(k==0 or prices.size()<=1) return 0;
-        // int n=prices.size();
-        // int dp[k+1][n];
-        // memset(dp,0,sizeof(dp));
-        // //case of zero transactions and case of n=0 (single element case), profit will surely be zero
-        // for(int kval=1;kval<=k;kval++){
-        //     int maxm=-prices[0];
-        //     for(int i=1;i<n;i++){
-        //         //int maxPro=dp[kval][j-1]; //I dont choose to transact on the current day..
-        //         int dontChose=dp[kval][i-1];
-        //         int currChose=prices[i]+maxm;
-        //         dp[kval][i]=max(currChose,dontChose);
-        //         //I keep on updating dp[kval-1][i] - prices[i]
-        //         maxm=max(maxm,-prices[i]+dp[kval-1][i]);
-        //     }
-        // }
-        // return dp[k][n-1];
     }
 };
