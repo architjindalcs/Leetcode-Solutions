be9250fab67cd5c4d415621ad3cb885879c4b299
Archit Jindal <59136349+architjindalcs@users.noreply.github.com>
2022-03-21 12:16:25 +0530

    Time: 95 ms (85.82%), Space: 11.7 MB (11.67%) - LeetHub

---
+++ 1395-count-number-of-teams/1395-count-number-of-teams.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int numTeams(vector<int>& nums) {
+        set<int> lset;
+        set<int> rset(nums.begin(),nums.end());
+        int res=0;
+        for(int i=1;i<nums.size();i++){
+            lset.insert(nums[i-1]);
+            rset.erase(nums[i-1]); 
+            //CASE 1 -> nums[i]<nums[j]<nums[k];
+            //1,2, 3
+            int d1=distance(lset.begin(),lset.lower_bound(nums[i]));
+            int d2=distance(rset.upper_bound(nums[i]),rset.end());
+            if(d1 > 0 && d2 > 0)
+                res += d1*d2;
+            
+            int d3 = std::distance(lset.upper_bound(nums[i]), lset.end()); 
+            int d4 = std::distance(rset.begin(), rset.lower_bound(nums[i])); 
+            if(d3 > 0 && d4 > 0)
+                res += d3*d4; 
+        }
+        return res;
+    }
+};
