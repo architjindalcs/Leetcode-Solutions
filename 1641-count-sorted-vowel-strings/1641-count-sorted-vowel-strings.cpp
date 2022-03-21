333cdbc38cbd6a4365de8cef9e919bb3657d28c8
Archit Jindal <59136349+architjindalcs@users.noreply.github.com>
2022-03-21 09:20:25 +0530

    Time: 4 ms (34.11%), Space: 5.9 MB (56.32%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
@@ -3,16 +3,18 @@ public:
     int countVowelStrings(int n) {
         int dp[n+1][5];
         memset(dp,0,sizeof(dp));
-        for(int i=0;i<=4;i++) dp[1][i]=i+1;
+        for(int i=0;i<=4;i++) dp[1][i]=1;  //dp[i][j] = number of strings of length i ending at vowel j..
         
         for(int len=2;len<=n;len++){
             
             int sum=0;
             for(int end=0;end<=4;end++){
-                sum+=(dp[len-1][end]);
+                sum+=(dp[len-1][end]);  //for eg: end=3, it should also include ending at 0,1,2,3 all, so taking sum for cumulative addition
                 dp[len][end]=sum;
             }
         }
-        return dp[n][4];
+        int ans=0;
+        for(int i=0;i<=4;i++) ans+=dp[n][i];
+        return ans;
     }
 };
