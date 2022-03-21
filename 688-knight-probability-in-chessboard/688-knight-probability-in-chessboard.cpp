d18d0afd6aa4cc8af8abf0d91be3e2bc8d837a83
Archit Jindal <59136349+architjindalcs@users.noreply.github.com>
2022-03-21 22:21:15 +0530

    Time: 15 ms (69.13%), Space: 6.6 MB (70.93%) - LeetHub

---
+++ 688-knight-probability-in-chessboard/688-knight-probability-in-chessboard.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    double knightProbability(int n, int k, int ro, int colum) {
+        double dp[k+1][n][n];
+        memset(dp,0.0,sizeof(dp));
+        int row[]={2,2,-2,-2,1,1,-1,-1};
+        int col[]={-1,1,1,-1,-2,2,2,-2};
+        dp[0][ro][colum]=1.0;
+        for(int mov=1;mov<=k;mov++){
+            for(int i=0;i<n;i++){
+                for(int j=0;j<n;j++){
+                    for(int k=0;k<8;k++){
+                        int newx=i+row[k],newy=j+col[k];
+                        if(newx>=0 and newx<n and newy>=0 and newy<n){
+                            dp[mov][i][j]+=(dp[mov-1][newx][newy]);
+                        }
+                    }
+                }
+            }
+        }
+        double ans=0.0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                ans+=(dp[k][i][j]);
+            }
+        }
+        return ans/(1.0*pow(8,k));
+    }
+};
