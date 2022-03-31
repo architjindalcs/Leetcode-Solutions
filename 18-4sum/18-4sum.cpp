536780898ef51a2be31db36a1c538b7e3b8dad3f
Archit Jindal <59136349+architjindalcs@users.noreply.github.com>
2022-03-31 09:52:42 +0530

    Time: 65 ms (74.16%), Space: 13.8 MB (30.29%) - LeetHub

--- 18-4sum/18-4sum.cpp
+++ 18-4sum/18-4sum.cpp
@@ -1,36 +1,39 @@
 class Solution {
 public:
-    vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        sort(nums.begin(),nums.end());
-        int n=nums.size();
-        if(nums.size()<4) return {};
-        vector<vector<int>> ans;
-        for(int i=0;i<nums.size()-3;i++){
-            //1,2,3,4 -> i<1
-            if(i>0 and nums[i]==nums[i-1]) continue;
-            if((long)nums[i]+(long)nums[i+1]+(long)nums[i+2]+(long)nums[i+3]>target) break;
-            if((long)nums[i]+(long)nums[n-1]+(long)nums[n-2]+(long)nums[n-3]<target) continue;
-            for(int j=i+1;j<nums.size()-2;j++){
-                if(j>i+1 and nums[j]==nums[j-1]) continue;               
-                if((long)nums[i]+(long)nums[j]+(long)nums[j+1]+(long)nums[j+2]>target) break;
-                if((long)nums[i]+(long)nums[j]+(long)nums[n-2]+(long)nums[n-1]<target) continue;  
-                int l=j+1,r=nums.size()-1;
-                while(l<r){
-                    int sum=nums[i]+nums[j]+nums[l]+nums[r];
-                    if(sum<target)
-                        l++;
-                    else if(sum>target)
-                        r--;
-                    else{
-                      //  cout<<i<<" "<<j<<" "<<l<<" "<<r<<endl;
-                        ans.push_back({nums[i],nums[j],nums[l],nums[r]});
-                        int ele1=nums[l],ele2=nums[r];
-                        while(l<r and nums[l]==ele1) l++;
-                        while(l<r and nums[r]==ele2) r--;
-                    }
+    vector<vector<int>> ksum(vector<int>& nums,int idx, int target,int k){
+        if(nums.size()<k) return {};
+        if(k==2){
+            vector<vector<int>> ans;
+            int l=idx,r=nums.size()-1;
+            while(l<r){
+                int sum=nums[l]+nums[r];
+                if(sum<target) l++;
+                else if(sum>target) r--;
+                else{
+                    ans.push_back({nums[l],nums[r]});
+                    int ele1=nums[l],ele2=nums[r];
+                    while(l<r and nums[l]==ele1) l++;
+                    while(l<r and nums[r]==ele2) r--;
                 }
+            }
+            return ans;
+        }
+        
+        vector<vector<int>> ans;
+        for(int i=idx;i<nums.size()-k+1;i++){
+            if(i>idx and nums[i]==nums[i-1]) continue;
+            vector<vector<int>> nans=ksum(nums,i+1,target-nums[i],k-1);
+            for(vector<int>& v: nans){
+                vector<int> temp;
+                temp.push_back(nums[i]);
+                for(int i: v) temp.push_back(i);
+                ans.push_back(temp);
             }
         }
         return ans;
+    }
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        sort(nums.begin(),nums.end());
+        return ksum(nums,0,target,4);
     }
 };
