a5214deb0adb50eae6741ee29c7a8d06e79a9c1c
Archit Jindal <59136349+architjindalcs@users.noreply.github.com>
2022-03-31 21:01:41 +0530

    Time: 16 ms (9.00%), Space: 7.3 MB (66.60%) - LeetHub

---
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        stack<int> st;
+        st.push(-1);
+        int ans=0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='('){
+                // -1, 
+                st.push(i);
+            }
+            else{
+                st.pop();
+                if(st.size()){
+                    ans=max(ans,i-st.top());
+                }
+                else st.push(i);
+            }
+        }
+        return ans;
+    }
+};
