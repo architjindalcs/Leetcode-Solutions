c01a9daa1b826adefde3b36c28a80917e44a3b2d
Archit Jindal <59136349+architjindalcs@users.noreply.github.com>
2022-03-31 21:32:10 +0530

    Time: 3 ms (88.28%), Space: 6.7 MB (99.49%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
@@ -1,22 +1,29 @@
 class Solution {
 public:
     int longestValidParentheses(string s) {
-        stack<int> st;
-        st.push(-1);
+        int n=s.size();
+        if(!n) return 0;
+        int dp[n];
+        memset(dp,0,sizeof(dp));
+        
         int ans=0;
-        for(int i=0;i<s.size();i++){
-            if(s[i]=='('){
-                // -1, 
-                st.push(i);
-            }
-            else{
-                st.pop();
-                if(st.size()){
-                    ans=max(ans,i-st.top());
+        for(int i=1;i<s.size();i++){
+            if(s[i]==')'){
+                
+                if(s[i-1]=='('){
+                    dp[i]=max(dp[i],2+(i>=2? dp[i-2]: 0));
                 }
-                else st.push(i);
+                else{
+                    // () () )
+                    int longestEndingAtPrevIndex=dp[i-1];
+                    if(i-longestEndingAtPrevIndex-1>=0 and 
+                       s[i-longestEndingAtPrevIndex-1]=='(')
+                    dp[i]=max(dp[i],longestEndingAtPrevIndex+2+(i-longestEndingAtPrevIndex-2>=0? dp[i-longestEndingAtPrevIndex-2] : 0));
+                }
             }
+            ans=max(ans,dp[i]);
         }
         return ans;
+       
     }
 };
