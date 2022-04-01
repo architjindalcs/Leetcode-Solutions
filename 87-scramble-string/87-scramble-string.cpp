0ae47362f8e4cda30d8598c240baaebef9e349e8
Archit Jindal <59136349+architjindalcs@users.noreply.github.com>
2022-04-01 09:45:43 +0530

    Time: 55 ms (73.98%), Space: 42.1 MB (13.58%) - LeetHub

--- 87-scramble-string/87-scramble-string.cpp
+++ 87-scramble-string/87-scramble-string.cpp
@@ -2,29 +2,28 @@ class Solution {
 public:
     unordered_map<string,bool> dp;
     bool isScramble(string s1, string s2) {
+        if(s1==s2) return true;
         string key=s1+"#"+s2;
-        int f1[26],f2[26];
-        if(s1==s2) return true;  //yes they are scrambled strings..
-        memset(f1,0,sizeof(f1));
-        memset(f2,0,sizeof(f2));
-        for(char ch: s1) f1[ch-'a']++;
-        for(char ch: s2) f2[ch-'a']++;
-        
-        for(int i=0;i<26;i++) {
-       //     temp+=(to_string(f1[i])+",");
-            if(f1[i]!=f2[i]) return dp[key]=false;
-        }
-        
         if(dp.count(key)) return dp[key];
-        
+        vector<int> f(256,0);
+        for(char ch: s1) f[ch]++;
+        for(char ch: s2) f[ch]--;
+        for(int i: f) if(i) return dp[key]=false;
+        /*
+            [g]reat   rgea[t]
+            cfs-> characters from string.
+        */
         int n=s1.size();
-        for(int i=0;i<s1.size()-1;i++){
-            if(isScramble(s1.substr(0,i+1),s2.substr(0,i+1)) and isScramble(s1.substr(i+1),s2.substr(i+1)))
-                return dp[key]=true;
-            int leftS1=(i+1),rightS1=s1.size()-(leftS1);
-            if(isScramble(s1.substr(0,leftS1),s2.substr(n-leftS1,leftS1)) and               
-               isScramble(s1.substr(n-rightS1,rightS1),s2.substr(0,rightS1))) 
-                return dp[key]=true;
+        for(int cfs=1;cfs<s1.size();cfs++){
+            bool c1=isScramble(s1.substr(0,cfs),s2.substr(0,cfs));
+            bool c2=isScramble(s1.substr(cfs),s2.substr(cfs));
+            
+            if(c1 and c2) return dp[key]=true;
+            
+            bool c3=isScramble(s1.substr(0,cfs),s2.substr(n-cfs));
+            bool c4=isScramble(s1.substr(cfs),s2.substr(0,n-cfs));
+            
+            if (c3 and c4) return dp[key]=true;
         }
         return dp[key]=false;
     }
