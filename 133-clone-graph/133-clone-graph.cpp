2d97d4ae8d996eb24491f74984b1371ae58dec1b
Archit Jindal <59136349+architjindalcs@users.noreply.github.com>
2022-04-01 23:16:19 +0530

    Time: 11 ms (35.49%), Space: 8.8 MB (61.09%) - LeetHub

--- 133-clone-graph/133-clone-graph.cpp
+++ 133-clone-graph/133-clone-graph.cpp
@@ -1,35 +1,19 @@
-/*
-// Definition for a Node.
-class Node {
-public:
-    int val;
-    vector<Node*> neighbors;
-    Node() {
-        val = 0;
-        neighbors = vector<Node*>();
-    }
-    Node(int _val) {
-        val = _val;
-        neighbors = vector<Node*>();
-    }
-    Node(int _val, vector<Node*> _neighbors) {
-        val = _val;
-        neighbors = _neighbors;
-    }
-};
-*/
-
 class Solution {
 public:
     unordered_map<Node*,Node*> map;
     Node* dfs(Node* node){
         if(!node) return NULL;
         Node* nn=new Node(node->val);
-        map[node]=nn;
+        map[node]=nn;  //kind of marking as visited..
         for(Node* nei: node->neighbors){
-            if(map.count(nei)) nn->neighbors.push_back(map[nei]);
+            if(map.count(nei)) {
+                //visited case, value is already present in map, directly push to neighbors the mapped value
+                nn->neighbors.push_back(map[nei]);
+            }
             else{
+                //not visited case, make a dfs call, to simulate the further process..
                 dfs(nei);
+                //now we may use map[nei], as mapping would have been created already.
                 nn->neighbors.push_back(map[nei]);
             }
         }
