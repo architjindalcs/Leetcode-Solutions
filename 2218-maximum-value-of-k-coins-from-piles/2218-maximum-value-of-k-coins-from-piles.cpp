58d11a3097621222fafb70cf346641835b4f2e69
Archit Jindal <59136349+architjindalcs@users.noreply.github.com>
2022-04-02 09:36:12 +0530

    Time: 176 ms (97.97%), Space: 15.1 MB (91.34%) - LeetHub

--- 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.cpp
+++ 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.cpp
@@ -1,24 +1,31 @@
 class Solution {
 public:
-    int dp[3001][3001];
-    int helper(vector<vector<int>>& piles,int idx,int k){
-        if(idx==piles.size()) return dp[idx][k]=0;  //invalid case..
-        
-        if(dp[idx][k]!=-1) return dp[idx][k];
-        
-        int skip=helper(piles,idx+1,k);
-        
-        int ans=skip;
-        
-        int cs=0;
-        for(int take=0;take<piles[idx].size() and k-(take+1)>=0;take++){
-            cs+=piles[idx][take];
-            ans=max(ans,cs+helper(piles,idx+1,k-(take+1)));    
-        }
-        return dp[idx][k]=ans;
-    }
     int maxValueOfCoins(vector<vector<int>>& piles, int k) {
-        memset(dp,-1,sizeof(dp));
-         return helper(piles,0,k);
+        int n=piles.size();
+        
+        int dp[n+1][k+1]; //coins ke lie..
+        
+        memset(dp,0,sizeof(dp));  //dp[0][i] for all i's -> 0 hi hoga, because piles hi nhi h
+        //dp[i] [0] for all i's zero hi hoga, coins nhi h 
+        
+        for(vector<int>& v: piles){
+            for(int i=1;i<v.size();i++){
+                v[i]+=v[i-1];
+            }
+        }
+        for(int i=1;i<=n;i++){
+            
+            for(int j=1;j<=k;j++){
+                dp[i][j]=dp[i-1][j]; //skipping the current pile completely..
+                
+                int maxTakeFromPile=min(j,(int)piles[i-1].size()); //maxm that can be taken from pile, j i.e. the coins..
+                for(int tfPile=1;tfPile<=maxTakeFromPile;tfPile++){
+                    int sum=(piles[i-1][tfPile-1]);
+                    dp[i][j]=max(dp[i][j], (sum) + dp[i-1][j-tfPile]);
+                }
+            }
+        }
+        return dp[n][k];
+        
     }
 };
