4ca3ee2ea198f2da93b9bce540e4c19b6ee09f1a
Archit Jindal <59136349+architjindalcs@users.noreply.github.com>
2022-04-02 11:30:12 +0530

    Time: 51 ms (52.40%), Space: 14.6 MB (82.80%) - LeetHub

--- 218-the-skyline-problem/218-the-skyline-problem.cpp
+++ 218-the-skyline-problem/218-the-skyline-problem.cpp
@@ -2,28 +2,24 @@ class Solution {
 public:
     vector<vector<int>> getSkyline(vector<vector<int>>& buildings) {
         vector<vector<int>> ans;
+        int currH=0;
         multiset<int> st;
         vector<pair<int,int>> v;
-        for(auto& vv: buildings){
-            int st=vv[0],end=vv[1],ht=vv[2];
-            v.push_back({st,-ht});
-            v.push_back({end,ht});
+        for(vector<int>& vv: buildings){
+            v.push_back({vv[0],-vv[2]});
+            v.push_back({vv[1],vv[2]});
         }
-        sort(v.begin(),v.end()); //sorted by x coordinates..
+        sort(v.begin(),v.end());
         st.insert(0);
-        int currHeight=0;
         for(int i=0;i<v.size();i++){
             int x=v[i].first,h=v[i].second;
-            if(h<0){
-                st.insert(-h);
-            }
-            else st.erase(st.find(h));  //erase the first instance only..
+            if(h<0) st.insert(-h);   //added
+            else st.erase(st.find(h));   //gone
             
-            
-            int biggestHeight=*(st.rbegin());
-            if(currHeight!=biggestHeight){
-                currHeight=biggestHeight;
-                ans.push_back({x,currHeight});
+            int maxH=*(st.rbegin());
+            if(maxH!=currH){
+                currH=maxH;
+                ans.push_back({x,currH});
             }
         }
         return ans;
