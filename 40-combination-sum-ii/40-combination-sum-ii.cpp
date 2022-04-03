8b989240dcd2d635b5f315dcfe8f84be88d59ffd
Archit Jindal <59136349+architjindalcs@users.noreply.github.com>
2022-04-03 18:33:06 +0530

    Time: 11 ms (43.39%), Space: 10.8 MB (45.00%) - LeetHub

---
+++ 40-combination-sum-ii/40-combination-sum-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+        vector<vector<int>> ans;
+    
+    void helper(vector<int>& nums,int idx,int cs,int target,vector<int>& path){
+        if(target==cs) ans.push_back(path);
+        if(idx==nums.size()){
+            return;
+        }
+        
+        for(int i=idx;i<nums.size();i++){
+            if(i>idx and nums[i]==nums[i-1]) continue;
+            if(cs+nums[i]<=target){
+                path.push_back(nums[i]);
+                helper(nums,i+1,cs+nums[i],target,path);
+                path.pop_back();
+            }
+        }
+    }
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+               vector<int> path;
+        sort(candidates.begin(),candidates.end());
+        helper(candidates,0,0,target,path);
+        return ans;
+    }
+};
