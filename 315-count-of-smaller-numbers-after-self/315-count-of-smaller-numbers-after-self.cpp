790377a6d3571066abfcaf4921965322340ed236
Archit Jindal <59136349+architjindalcs@users.noreply.github.com>
2022-04-03 10:17:39 +0530

    Time: 483 ms (68.19%), Space: 75 MB (89.33%) - LeetHub

--- 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.cpp
+++ 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.cpp
@@ -2,13 +2,19 @@ class Solution {
 public:
     vector<int> indices;
     vector<int> ans;
-    void merge(vector<int>& nums,int l,int r,int m){
+    
+    void merge(vector<int>& nums,int l,int m,int r){
         int n1=m-l+1,n2=r-m;
         int L[n1],R[n2];
+        memset(L,0,sizeof(L));
+        memset(R,0,sizeof(R));
+        
         for(int i=0;i<n1;i++) L[i]=indices[l+i];
         for(int i=0;i<n2;i++) R[i]=indices[m+1+i];
-        int i=0,j=0,k=l;
+        
         int cnt=0;
+        
+        int i=0,j=0,k=l;
         while(i<n1 and j<n2){
             if(nums[L[i]]<=nums[R[j]]){
                 ans[L[i]]+=cnt;
@@ -19,7 +25,8 @@ public:
                 indices[k++]=R[j++];
             }
         }
-        while(i<n1) {
+        
+        while(i<n1){
             ans[L[i]]+=cnt;
             indices[k++]=L[i++];
         }
@@ -32,13 +39,15 @@ public:
             int m=(l+r)/2;
             mergeSort(nums,l,m);
             mergeSort(nums,m+1,r);
-            merge(nums,l,r,m);
+            merge(nums,l,m,r);
         }
     }
     vector<int> countSmaller(vector<int>& nums) {
-        for(int i=0;i<nums.size();i++) indices.push_back(i);
-        ans=vector<int> (nums.size(),0);
-        mergeSort(nums,0,nums.size()-1);
+        int n=nums.size();
+        indices=vector<int> (n,0);
+        for(int i=0;i<indices.size();i++) indices[i]=i;
+        ans=vector<int> (n,0);
+        mergeSort(nums,0,n-1);
         return ans;
     }
 };
