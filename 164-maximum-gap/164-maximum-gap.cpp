0cc291cc5e5558052feeaf3697d40d27739fcf59
Archit Jindal <59136349+architjindalcs@users.noreply.github.com>
2022-04-03 17:21:01 +0530

    Time: 298 ms (36.15%), Space: 78.4 MB (34.93%) - LeetHub

--- 164-maximum-gap/164-maximum-gap.cpp
+++ 164-maximum-gap/164-maximum-gap.cpp
@@ -1,31 +1,34 @@
 class Solution {
 public:
     int maximumGap(vector<int>& nums) {
-        int maxm=nums[0],minm=nums[0];
+        //https://leetcode.com/problems/maximum-gap/discuss/1241681/JavaPython-Bucket-Idea-with-Picture-Clean-and-Concise-O(N)
+        vector<int> minArr(nums.size(),INT_MAX);
+        vector<int> maxArr(nums.size(),INT_MIN);
+        int minm=INT_MAX,maxm=INT_MIN;
         for(int i: nums) {
-            maxm=max(maxm,i);
             minm=min(minm,i);
+            maxm=max(maxm,i);
         }
-        int n=nums.size();
         if(maxm==minm) return 0;
-        int bucketSize=(maxm-minm)%(n-1)==0? (maxm-minm)/(n-1) : (maxm-minm)/(n-1)+1;
+        int n=nums.size(); 
+        int gap=(maxm-minm)%(n-1) ==0 ? (maxm-minm)/(n-1) : ((maxm-minm)/(n-1)) + 1;
+        //gap -> minimum value of maximum possible gap in array, means maxium gap >= gap
         
-        vector<int> maxArr(n,INT_MIN);
-        vector<int> minArr(n,INT_MAX);
-        for(int ele: nums){
-            int idx=(ele-minm)/(bucketSize);
-            maxArr[idx]=max(maxArr[idx],ele);
-            minArr[idx]=min(minArr[idx],ele);
+        //say max= 45 min=3 and n=7 -> (42/6) => gap=7  this is the minimum value of maximum possible gap.
+        //so assign from [3-9] in bucket 0 and [10-16] in bucket 0 and so on..
+        //also, b/w the elements of a bucket, our answer can't be there because there gap is less than minm value of maxgap for sure.
+        
+        for(int num: nums){
+            int bucketIdx=(num-minm)/gap;
+            minArr[bucketIdx]=min(minArr[bucketIdx],num);
+            maxArr[bucketIdx]=max(maxArr[bucketIdx],num);
         }
         
-        int prev=INT_MIN;
-        int maxGap=INT_MIN;
+        int prevMax=INT_MIN, maxGap=INT_MIN;
         for(int i=0;i<n;i++){
-            if(maxArr[i]==INT_MIN) continue; 
-            if(prev!=INT_MIN){
-                maxGap=max(maxGap,minArr[i]-prev);
-            }
-            prev=maxArr[i];
+            if(minArr[i]==INT_MAX) continue;  //no element falled in this bucket..
+            if(prevMax!=INT_MIN) maxGap=max(maxGap,minArr[i]-prevMax);
+            prevMax=maxArr[i];
         }
         return maxGap;
     }
