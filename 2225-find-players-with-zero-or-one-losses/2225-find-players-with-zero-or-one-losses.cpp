cbd048d1628f83b5e9789e6a21ac659e44a27bbe
Archit Jindal <59136349+architjindalcs@users.noreply.github.com>
2022-04-05 22:02:54 +0530

    Time: 1011 ms (59.16%), Space: 191.4 MB (36.67%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
@@ -1,27 +1,20 @@
 class Solution {
 public:
     vector<vector<int>> findWinners(vector<vector<int>>& matches) {
-        unordered_set<int> all;
+        unordered_map<int,int> indeg,outdeg;
         for(vector<int>& v: matches) {
-            all.insert(v[0]);
-            all.insert(v[1]);
+            outdeg[v[0]]++;
+            if(indeg.count(v[0])==0) indeg[v[0]]=0;
+            indeg[v[1]]++;
+            if(outdeg.count(v[1])==0) outdeg[v[1]]=0;
         }
-        unordered_set<int> allwin=all;
-        unordered_map<int,int> flose;
-        
-        for(vector<int>& v: matches){
-            if(allwin.count(v[1])) allwin.erase(v[1]);
-            flose[v[1]]++;
+        vector<vector<int>> ans(2);
+        for(auto x: indeg){
+            if(x.second==0) ans[0].push_back(x.first);
+            if(x.second==1) ans[1].push_back(x.first);
         }
         
-        vector<vector<int>> ans;
-        ans.push_back(vector<int> (allwin.begin(),allwin.end()));
         
-        vector<int> v;
-        for(auto x: flose)
-            if(x.second==1) v.push_back(x.first);
-        
-        ans.push_back(v);
         sort(ans[0].begin(),ans[0].end());
         sort(ans[1].begin(),ans[1].end());
         return ans;
