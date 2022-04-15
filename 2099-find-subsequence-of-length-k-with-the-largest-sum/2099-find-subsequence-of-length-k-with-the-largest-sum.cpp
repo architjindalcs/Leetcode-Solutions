87e55454757a8baeff2a64a4c493e6143a871d96
Archit Jindal <59136349+architjindalcs@users.noreply.github.com>
2022-04-15 12:58:45 +0530

    Time: 14 ms (34.47%), Space: 9.1 MB (98.02%) - LeetHub

--- 2099-find-subsequence-of-length-k-with-the-largest-sum/2099-find-subsequence-of-length-k-with-the-largest-sum.cpp
+++ 2099-find-subsequence-of-length-k-with-the-largest-sum/2099-find-subsequence-of-length-k-with-the-largest-sum.cpp
@@ -2,41 +2,19 @@ class Solution {
 public:
     vector<int> maxSubsequence(vector<int>& nums, int k) {
         vector<int> ans;
-        int maxSum=INT_MIN;
-        for(int i=0;i<nums.size();i++){
-            int num=nums[i];
-            
-            priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>> > pq;
-            int aageKaSum=0,siz=k-1;
-            for(int j=i+1;j<nums.size();j++){
-                if(pq.size()<siz){
-                    pq.push({nums[j],j});
-                    aageKaSum+=nums[j];
-                }
-                else{
-                    aageKaSum+=nums[j];
-                    pq.push({nums[j],j});
-                    aageKaSum-=pq.top().first;
-                    pq.pop();
+        for(int i=0;i<k;i++) ans.push_back(nums[i]);
+        for(int j=k;j<nums.size();j++){
+            int minm=INT_MAX,minIdx=-1;
+            for(int i=0;i<ans.size();i++){
+                if(ans[i]<minm){
+                    minm=ans[i];
+                    minIdx=i;
                 }
             }
-            
-            int tsum=num+aageKaSum;
-
-            if(tsum>maxSum and pq.size()==siz){
-                ans.clear();
-                maxSum=tsum;
-                vector<int> tidx;
-                while(pq.size()) {
-                    tidx.push_back(pq.top().second);
-                    pq.pop();
-                }
-                tidx.push_back(i);
-                sort(tidx.begin(),tidx.end());
-                for(int idx: tidx) ans.push_back(nums[idx]);
-            }
+            if(nums[j]<minm) continue;
+            ans.erase(ans.begin()+minIdx);
+            ans.push_back(nums[j]);
         }
-//        cout<<"maxSum: "<<maxSum<<endl;
         return ans;
     }
 };
