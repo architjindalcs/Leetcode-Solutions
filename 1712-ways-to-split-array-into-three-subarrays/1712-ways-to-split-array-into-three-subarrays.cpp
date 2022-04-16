6925da70e4bfe185fedfb649866e573a3f47ba29
Archit Jindal <59136349+architjindalcs@users.noreply.github.com>
2022-04-16 15:18:45 +0530

    Time: 497 ms (16.32%), Space: 84.9 MB (65.76%) - LeetHub

--- 1712-ways-to-split-array-into-three-subarrays/1712-ways-to-split-array-into-three-subarrays.cpp
+++ 1712-ways-to-split-array-into-three-subarrays/1712-ways-to-split-array-into-three-subarrays.cpp
@@ -1,44 +1,34 @@
 class Solution {
 public:
-    
     int helper(vector<int>& nums,vector<int>& pre,int stidx,bool left){
+        int n=nums.size();
         int st=stidx,end=nums.size()-2;
         int idx=-1;
         int leftSum=pre[stidx-1];
         while(st<=end){
-            int mid=st+(end-st)/2;
-            //[0.......st-1][st ......... mid][mid+1......nums.size()-1]
-            
-            int midSum=pre[mid]-(pre[stidx-1]);
-            int rightSum=pre.back()-(midSum+leftSum);
+            int mid=(st+(end-st)/2);
+            int midSum=(pre[mid]-(pre[stidx-1]));  // [0......stidx-1] [stidx.......mid] [mid+1.......nums.size()-1]
+            int rightSum=(pre.back()-midSum-leftSum);
             if(leftSum<=midSum and midSum<=rightSum){
                 idx=mid;
-                if(left) end=mid-1;  //search on the left
-                else st=mid+1;   //search on the right..
+                if(left) end=mid-1;
+                else st=mid+1;
             }
-            else if(midSum<leftSum){
-                //we need to increment midSum
+            else if(leftSum>midSum){
                 st=mid+1;
             }
-            else end=mid-1;
+            else end=mid-1;  
         }
         return idx;
     }
     int waysToSplit(vector<int>& nums) {
-        /*
-            []
-        
-        */
+        long ans=0;
+        int mod=1000000007;
         int n=nums.size();
         vector<int> pre(n);
         pre[0]=nums[0];
-        for(int i=1;i<nums.size();i++){
-            pre[i]=nums[i]+pre[i-1];
-        }
-        long ans=0;
-        int mod=1000000007;
+        for(int i=1;i<nums.size();i++) pre[i]=nums[i]+pre[i-1];
         for(int i=1;i<nums.size()-1;i++){
-            if(pre[i-1]>(pre[n-1]-pre[i-1])/2) break;
             int left=helper(nums,pre,i,true);
             int right=helper(nums,pre,i,false);
             if(left==-1 or right==-1) continue;
