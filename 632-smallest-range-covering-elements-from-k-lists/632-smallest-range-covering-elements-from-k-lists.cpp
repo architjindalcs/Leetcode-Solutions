676f041739bf19a31beffec308e4c237e5cc9f32
Archit Jindal <59136349+architjindalcs@users.noreply.github.com>
2022-04-17 16:27:04 +0530

    Time: 69 ms (65.37%), Space: 15.7 MB (55.98%) - LeetHub

--- 632-smallest-range-covering-elements-from-k-lists/632-smallest-range-covering-elements-from-k-lists.cpp
+++ 632-smallest-range-covering-elements-from-k-lists/632-smallest-range-covering-elements-from-k-lists.cpp
@@ -3,37 +3,38 @@ public:
     vector<int> smallestRange(vector<vector<int>>& nums) {
         vector<int> ans;
         priority_queue<int> maxHeap;
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>> > minHeap;
-        int k=nums.size();
-        vector<int> nI(k,1);
-        for(int i=0;i<k;i++){
+        priority_queue<pair<int,int>,vector<pair<int,int> > , greater<pair<int,int>> > minHeap;
+        int minv,maxv,minDiff=INT_MAX;
+        for(int i=0;i<nums.size();i++){
             minHeap.push({nums[i][0],i});
             maxHeap.push(nums[i][0]);
         }
-        int minDiff=INT_MAX;
-        int minv,maxv;
+        vector<int> nI(nums.size(),1);
         while(minHeap.size()){
-            pair<int,int> fr=minHeap.top();
-            minHeap.pop();
-            int nodeVal=fr.first;
-            int listNo=fr.second;
-            int minVal=nodeVal;
+            int minVal=minHeap.top().first;
+            int listNo=minHeap.top().second;
             int maxVal=maxHeap.top();
+            minHeap.pop();
             if(maxVal-minVal<minDiff){
                 minDiff=maxVal-minVal;
                 minv=minVal;
                 maxv=maxVal;
             }
-            
+            else if(maxVal-minVal==minDiff){
+                if(minVal<minv){
+                    minv=minVal;
+                    maxv=maxVal;
+                }
+            }
             int nidx=nI[listNo];
             if(nidx<nums[listNo].size()){
                 minHeap.push({nums[listNo][nidx],listNo});
-                maxHeap.push(nums[listNo][nidx]);
                 nI[listNo]++;
+                maxHeap.push(nums[listNo][nidx]);
             }
             else break;
         }
-        return {minv,maxv};
         
+        return {minv,maxv};
     }
 };
