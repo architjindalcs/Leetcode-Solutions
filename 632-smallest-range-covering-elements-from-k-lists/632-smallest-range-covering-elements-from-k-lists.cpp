934c33f206766751774b90e8df3fe1e05f860c28
Archit Jindal <59136349+architjindalcs@users.noreply.github.com>
2022-04-17 17:00:27 +0530

    Time: 56 ms (88.66%), Space: 14.5 MB (72.52%) - LeetHub

--- 632-smallest-range-covering-elements-from-k-lists/632-smallest-range-covering-elements-from-k-lists.cpp
+++ 632-smallest-range-covering-elements-from-k-lists/632-smallest-range-covering-elements-from-k-lists.cpp
@@ -2,18 +2,18 @@ class Solution {
 public:
     vector<int> smallestRange(vector<vector<int>>& nums) {
         vector<int> ans;
-        priority_queue<int> maxHeap;
+        int maxm=INT_MIN;            
         priority_queue<pair<int,int>,vector<pair<int,int> > , greater<pair<int,int>> > minHeap;
         int minv,maxv,minDiff=INT_MAX;
         for(int i=0;i<nums.size();i++){
             minHeap.push({nums[i][0],i});
-            maxHeap.push(nums[i][0]);
+            maxm=max(maxm,nums[i][0]);
         }
         vector<int> nI(nums.size(),1);
         while(minHeap.size()){
             int minVal=minHeap.top().first;
             int listNo=minHeap.top().second;
-            int maxVal=maxHeap.top();
+            int maxVal=maxm;
             minHeap.pop();
             if(maxVal-minVal<minDiff){
                 minDiff=maxVal-minVal;
@@ -30,7 +30,7 @@ public:
             if(nidx<nums[listNo].size()){
                 minHeap.push({nums[listNo][nidx],listNo});
                 nI[listNo]++;
-                maxHeap.push(nums[listNo][nidx]);
+                maxm=max(maxm,nums[listNo][nidx]);
             }
             else break;
         }
