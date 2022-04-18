172e8c947ed75aa9a6c9ff4c27f7a52b5ae33c34
Archit Jindal <59136349+architjindalcs@users.noreply.github.com>
2022-04-18 15:50:19 +0530

    Time: 760 ms (36.49%), Space: 8.7 MB (67.44%) - LeetHub

--- 786-k-th-smallest-prime-fraction/786-k-th-smallest-prime-fraction.cpp
+++ 786-k-th-smallest-prime-fraction/786-k-th-smallest-prime-fraction.cpp
@@ -18,7 +18,7 @@ public:
         priority_queue<pair<int,int>,vector<pair<int,int>>, decltype(cmp) > pq(cmp);
         
         int n=arr.size();
-        for(int i=0;i<arr.size()-1;i++) pq.push({i,arr.size()-1});
+        for(int i=0;i<arr.size()-1 and i<=k-1;i++) pq.push({i,arr.size()-1});
         
         vector<int> ans;
         while(pq.size()){
