976ce68b6202593f746b145995d1a0d55720ac6d
Archit Jindal <59136349+architjindalcs@users.noreply.github.com>
2022-04-18 22:33:01 +0530

    Time: 160 ms (85.76%), Space: 47.3 MB (51.20%) - LeetHub

--- 802-find-eventual-safe-states/802-find-eventual-safe-states.cpp
+++ 802-find-eventual-safe-states/802-find-eventual-safe-states.cpp
@@ -1,35 +1,34 @@
 class Solution {
 public:
-    vector<int> color;
-    bool dfs(vector<vector<int>>& adj,int src){
-        color[src]=2; //marked initially as unsafe..
+    bool safe(vector<vector<int>>& adj,int src,vector<int>& color){
+        /*
+            color[i] = -1, unvisited
+            color[i] = 0, eventually safe
+            color[i] = 1, unsafe state
+        */
+        color[src]=1;  //marking it as unsafe for the time being..
         for(int i: adj[src]){
-            if(color[i]==2) return false; //bychance, we get here..it will lead to cycle, so 
-            //unsafe... 
-            /*
-                1--->2--->3
-                ^
-                |_ _ _ _ _|
-                in above case, 1 is eventually unsafe node..
-            */
-            if(color[i]==0){
-                if(!dfs(adj,i)){
-                    return false;  //it is unsafe node...
+            if(color[i]==1) return false;  //it is not safe state, because leading to unsafe node..
+            if(color[i]==-1){
+                //not visited node..
+                if(!safe(adj,i,color)){
+                    return false;
                 }
             }
         }
-        color[src]=1;
+        color[src]=0;
         return true;
     }
-    vector<int> eventualSafeNodes(vector<vector<int>>& adj) {
-        int n=adj.size();
-        color=vector<int> (n,0);
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        int n=graph.size();
+        vector<int> color(n,-1);
         vector<int> ans;
         for(int i=0;i<n;i++){
-            if(color[i]==0){
-                if(dfs(adj,i)) ans.push_back(i);
+            if(color[i]==-1){
+                if(safe(graph,i,color))
+                ans.push_back(i);    
             }
-            else if(color[i]==1) ans.push_back(i);
+            else if(color[i]==0) ans.push_back(i);
         }
         return ans;
     }
