f8056b85fab9593073cc8403d9dbeb3b966f20a1
Archit Jindal <59136349+architjindalcs@users.noreply.github.com>
2022-04-18 22:13:38 +0530

    Time: 201 ms (79.80%), Space: 91 MB (75.49%) - LeetHub

--- 801-minimum-swaps-to-make-sequences-increasing/801-minimum-swaps-to-make-sequences-increasing.cpp
+++ 801-minimum-swaps-to-make-sequences-increasing/801-minimum-swaps-to-make-sequences-increasing.cpp
@@ -15,7 +15,7 @@ public:
             */
             
             if(nums1[i]>nums1[i-1] and nums2[i]>nums2[i-1]){
-                swap[i]=min(swap[i],1+swap[i-1]);
+                swap[i]=1+swap[i-1];
                 noswap[i]=noswap[i-1];
             }
             
